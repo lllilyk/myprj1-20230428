b8530114e6167ebff191ab967fc43f53588db248
lllilyk <ym0402kkk@naver.com>
2023-04-28 18:46:50 +0900

    test

---
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/")
+public class BoardController {
+
+}
