ed45760d0728374f5c39616bfaa96332e58516b0
lllilyk <ym0402kkk@naver.com>
2023-04-28 18:42:17 +0900

    first

---
+++ src/main/java/com/example/demo/Myprj120230428Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myprj120230428Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myprj120230428Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Myprj120230428ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myprj120230428ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
